882eef62acf6ef22ca986d446d2266ef03314193
raquelmu <raquel.muniesa@gmail.com>
2020-06-04 17:33:54 +0200

    fix cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(
 	cors({
 		credentials: true,
-		origin: [process.env.FRONTEND_DOMAIN, "http://localhost:3002"],
+		origin: [process.env.FRONTEND_DOMAIN],
 	})
 );
 app.use(logger('dev'));
