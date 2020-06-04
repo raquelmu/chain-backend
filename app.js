351d72e19fa03e8508f761fa00d60fc564b57f14
raquelmu <raquel.muniesa@gmail.com>
2020-06-04 17:30:04 +0200

    fix cors

--- app.js
+++ app.js
@@ -59,6 +59,21 @@ app.use(
 	})
 );
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 app.use('/', authRouter);
 app.use('/ads', adsRouter);
 app.use('/user', userRouter);
