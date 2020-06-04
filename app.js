7b71c883995b65210b764f8cc00eca926f692cd0
raquelmu <raquel.muniesa@gmail.com>
2020-06-04 17:54:58 +0200

    fix cors

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const userRouter = require('./routes/user')
 const app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'chain-app-frontend.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
